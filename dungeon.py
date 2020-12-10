ee36b91f73ea73807f05a665c58c80d12d976620
Sriba Rajendran <sribarajendran@gmail.com>
2020-12-09 17:31:38 -0800

    Reconfigured the dungeon with Tom's code and randomly assigned an entrance and an exit and blocked 5 rooms

--- dungeon.py
+++ dungeon.py
@@ -5,84 +5,131 @@ class Room():
     """
       creates a room.
     """
-    def __init__(self,a,b):
-        self.row = a
-        self.col = b
-        #corresponds to the four doors North, South, West and East.
-        #initially all doors are open and so set to true.
-        self.doors = [True, True, True, True]
+    def __init__(self):
+        self.__healthPotion = False
+        self.__visionPotion = False
+        self.__pillar = "No pillar"
+        self.__pit = False
+        self.__exit = False
+        self.__entrance = False
+        self.__impassable = False
+        self.__visited = False
+        self.__healthChance = 10
+
+    def get_health_chance(self):
+        return self.__healthChance
 
     def __str__(self):
-        return (f"room:({self.row},{self.col})")
+        item_count = 0;
+        if self.__healthPotion:
+            item_count += 1
+        if self.__visionPotion:
+            item_count += 1
 
-class RoomFactory():
-    def create_rooms(r,c):
-        """
-        Returns a list of lists consisting of rooms with the doors in the outer edges set to False(closed).
-        :param r: the number of rows
-        :param c: the number of columns
-        :return: list of lists of rows
-        """
-        rooms = []
-        for num1 in range(r):
-            rows = []
-            for num2 in range(c):
-                rows.append(Room(num1,num2))
-            rooms.append(rows)
-        #establishing the closed doors for the rooms in the outer borders.
-        for room in rooms[0]:
-            #setting north to False for rooms in first row
-            room.doors[0] = False
-        for room in rooms[r-1]:
-            #setting south to False for rooms in last row
-            room.doors[1] = False
-        for num in range(r-1):
-            #setting west to False for rooms in first column
-            rooms[num][0].doors[2] = False
-        for num in range(r-1):
-            #setting east to False for rooms in last column
-            rooms[num][c-1].doors[3] = False
-        return rooms
+
+        if item_count > 1:
+            return "M"
+
+        return "Health potion: " + str(self.__healthPotion) + "\n" \
+               + "Vision potion: " + str(self.__visionPotion) + "\n" \
+               + "Pillar: " + str(self.__pillar) + "\n" \
+               + "Pit: " + str(self.__pit) + "\n" \
+               + "Entrance: " + str(self.__entrance) + "\n" \
+               + "Exit: " + str(self.__exit) + "\n" \
+               + "Impassable: " + str(self.__impassable) + "\n\n"
+
+
+
+    def set_health(self, add_potion):
+        self.__healthPotion = add_potion
+
+    def can_enter(self):
+        return not self.__impassable and not self.__visited
+
+    def is_entrance(self):
+        return self.__entrance
+
+    def is_exit(self):
+        return self.__exit
+
+    def is_impassable(self):
+        return self.__impassable
+
+    def set_visited(self, visited):
+        self.__visited = visited
+
+    def set_entrance(self):
+        self.__entrance = True
+
+    def set_exit(self):
+        self.__exit = True
+
+    def set_impassable(self):
+        self.__impassable = True
+
 
 class Dungeon():
     """
       builds a dungeon
     """
-    def __init__(self,r,c):
-        self.rows = r
-        self.columns = c
-        self.grid = RoomFactory.create_rooms(self.rows,self.columns)
-        self.entrance = self.grid[0][0]
-        self.exit = self.grid[r-1][c-1]
+    def __init__(self, row_count = 5, col_count = 5, block = 5):
+        self.__maze = []
+        self.__rowCount = row_count
+        self.__colCount = col_count
+        self.__block = block
+        # Adding rooms to the maze (Default 2D grid of rooms)
+        for r in range(0, self.__rowCount):
+            self.__maze.append([Room() for c in range(0, self.__colCount)])
+        # To set entrance, exit, blocks and additional features to the maze:
+        self.build_maze()
+
+    def build_maze(self):
+        # To randomly set an entrance and exit
+        rand_row = random.choice(range(0,self.__rowCount))
+        rand_col = random.choice(range(0,self.__colCount))
+        self.__maze[rand_row][rand_col].set_entrance()
+        print(f"entrance:[{rand_row}][{rand_col}]")
+        count = 0
+        while count == 0:
+            rand_row = random.choice(range(0,self.__rowCount))
+            rand_col = random.choice(range(0,self.__colCount))
+            if not self.__maze[rand_row][rand_col].is_entrance():
+                self.__maze[rand_row][rand_col].set_exit()
+                print(f"exit:[{rand_row}][{rand_col}]")
+                count += 1
+        # make some rooms impassible (making 5 rooms impassable)
+        block = 0
+        while block < self.__block:
+            rand_row = random.choice(range(0,self.__rowCount))
+            rand_col = random.choice(range(0,self.__colCount))
+            if not self.__maze[rand_row][rand_col].is_entrance() and \
+                    not self.__maze[rand_row][rand_col].is_exit() and \
+                    not self.__maze[rand_row][rand_col].is_impassable():
+                self.__maze[rand_row][rand_col].set_impassable()
+                block += 1
+                print(f"blocked room at [{rand_row}][{rand_col}]")
+
 
     def __str__(self):
         out_li = []
-        for elem in self.grid:
+        for elem in self.__maze:
             out = []
             for item in elem:
                 out.append(item.__str__())
             out_li.append(",".join(out))
         return "\n".join(out_li)
 
-    def gen_maze(self):
-        count =0
-        while count < 5:
-            random_row = random.choice(range(0,self.rows))
-            random_col = random.choice(range(0,self.columns))
-            random_room = self.grid[random_row][random_col]
-            random_door = random.choice(range(0,4))
-            if not random_room.doors[random_door]:
-                continue
-            else:
-                random.choice(range(0,self.columns))
-                print(random_row,random_col)
-                random_room.doors[random_door] = False
-                count += 1
-            print("deleted",random_room.doors)
+    def print_maze(self):
+        # print(self.__maze)
+        for row in range(0, self.__rowCount):
+            print("row ", row)
+            for col in range(0, self.__colCount):
+                print(self.__maze[row][col].__str__())
+            print()
 
     def traversable_bfs(self):
         queue = deque()
-        queue.append(self.grid[0][0])
+        queue.append(self.__maze[0][0])
         while len(queue)>0:
             node = queue.popleft()
             if node == self.exit:
@@ -98,6 +145,7 @@ class Dungeon():
         return False
 
 d1 = Dungeon(5,5)
-print(d1.__str__())
-d1.gen_maze()
-print("Traversable:",d1.traversable_bfs())
+d1.print_maze()
+print("printing",d1)
+#d1.gen_maze()
+#print("Traversable:",d1.traversable_bfs())
