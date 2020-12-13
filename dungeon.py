8770f8aa5afd5db98533d50bedcc96808582a552
Sriba Rajendran <sribarajendran@gmail.com>
2020-12-12 17:59:41 -0800

    Add adventurer position in the maze and methods that update the adventurer's pos depending on user input.

--- dungeon.py
+++ dungeon.py
@@ -100,6 +100,7 @@ class Dungeon():
         self.entrance_pos = None
         self.exit_pos = None
         self.build_maze()
+        self.adventurer_pos = self.entrance_pos
 
     def pick_random_empty_room(self):
         """
@@ -115,6 +116,7 @@ class Dungeon():
             if rand_room.is_empty() and not rand_room.is_entrance() \
                     and not rand_room.is_exit() and not rand_room.is_impassable():
                 return rand_row,rand_col
+
 
     def set_entrance(self):
         """
@@ -165,6 +167,7 @@ class Dungeon():
           Ensures exit is reachable from the entrance and updates
           visited field of all traverse-able rooms in the maze.
         """
+        self.__maze = []
         # Adding rooms to the maze (Default 2D grid of rooms)
         for r in range(0, self.__rowCount):
             self.__maze.append([Room() for c in range(0, self.__colCount)])
@@ -218,6 +221,7 @@ class Dungeon():
 
     def is_valid_room(self, row, col):
         """
+          Helper function to check maze traversal.
           Returns true if row,col is a valid room in the maze and if it can be entered.
           (not blocked and not visited)
         :param row: Int index
@@ -226,6 +230,68 @@ class Dungeon():
         """
         return 0 <= row < self.__rowCount and 0 <= col < self.__colCount and self.__maze[row][col].can_enter()
 
+    def update_adv_pos(self,value):
+        """
+          Updates the position of the adventurer in the maze depending on the user input
+        :return: Updated(current) position of the adventurer after the move.
+        """
+        if value == "N" or value == "n":
+            print("moving north")
+            if self.check_room(self.adventurer_pos[0]-1,self.adventurer_pos[1]):
+                print("upd")
+                self.adventurer_pos = (self.adventurer_pos[0]-1,self.adventurer_pos[1])
+            else:
+                print("Cannot enter this location")
+        elif value == "S" or value == "s":
+            print("moving south")
+            if self.check_room(self.adventurer_pos[0]+1,self.adventurer_pos[1]):
+                print("upd")
+                self.adventurer_pos = (self.adventurer_pos[0]+1,self.adventurer_pos[1])
+            else:
+                print("Cannot enter this location")
+        elif value == "W" or value == "w":
+            print("moving west")
+            if self.check_room(self.adventurer_pos[0],self.adventurer_pos[1]-1):
+                print("upd")
+                self.adventurer_pos = (self.adventurer_pos[0],self.adventurer_pos[1]-1)
+            else:
+                print("Cannot enter this location")
+        elif value == "E" or value == "e":
+            print("moving east")
+            if self.check_room(self.adventurer_pos[0],self.adventurer_pos[1]+1):
+                print("upd")
+                self.adventurer_pos = (self.adventurer_pos[0],self.adventurer_pos[1]+1)
+            else:
+                print("Cannot enter this location")
+
+    def check_room(self, row, col):
+        """
+          Helper function to see room exists and can be entered by the adventurer.
+          Returns true if row,col is a valid room in the maze and if it can be entered.
+          (not blocked)
+        :param row: Int index
+        :param col: Int index
+        :return: Bool
+        """
+        return 0 <= row < self.__rowCount and 0 <= col < self.__colCount and not self.__maze[row][col].is_impassable()
+
+    def display_curr_room(self):
+        """
+          Display the current room
+        :return: String representation of the current room
+        """
+        print("Current room:",self.__maze[self.adventurer_pos[0]][self.adventurer_pos[1]])
+
+    def get_user_input(self):
+        while True:
+            try:
+                self.display_curr_room()
+                user_input = str(input("Enter the direction you want to move(N, S, E, W):"))
+                self.update_adv_pos(user_input)
+            except ValueError:
+                print("Error: Invalid input type.")
 
 d1 = Dungeon(5,5)
 print(d1)
+while True:
+    d1.get_user_input()
