1f55ffd0baea476ec8375a59fb6f38fd76e64a32
Sriba Rajendran <sribarajendran@gmail.com>
2020-12-16 23:42:08 -0800

    Add documentation.

--- room_factory.py
+++ room_factory.py
@@ -8,8 +8,15 @@ OBSTACLE_CHANCE = 10
 
 
 class RoomFactory():
+    """
+      This factory creates the rooms that go in the dungeon.
+    """
     @staticmethod
     def create_room():
+        """
+         Creates a room object and places a healing potion, vision potion
+         and/or an obstacle in it depending on the random chances.
+        """
         room = Room()
         if randint(0, 100) < HEALING_CHANCE:
             room.add_feature(RoomFeatureFactory.create_healing_potion())
